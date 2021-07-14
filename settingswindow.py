d44879c3974d0e7f40252cb9574c7b27f54b5162
retrontology <retrontology@hotmail.com>
2021-07-14 00:42:41 -0500

    stop other setting window hotkey entries

--- settingswindow.py
+++ settingswindow.py
@@ -27,6 +27,7 @@ class SettingsWindow(tk.Toplevel):
         tk.Label(hotkey_frame, text="'Stop All Sounds' hotkey(s):", justify='left').grid(column=0, row=0, sticky='w')
         self.stopall = HotkeyEntry(hotkey_frame, clearable=False, stored=self.master.hotkey_listener.get_hotkey('stop_all', HotkeyScope.GLOBAL)._keys, stop_callback=lambda x: self.bind_global_hotkey('stop_all', x, self.master.stop_all), textvariable=self.master.stopall_var)
         self.stopall.grid(column=1, row=0, sticky='ew')
+        self.stopall.bind('<Button>', lambda x: self.stop_other_entries(self.stopall), '+')
         # Push To Talk
         tk.Label(hotkey_frame, text="VoIP 'Push To Talk' enabled:", justify='left').grid(column=0, row=1, sticky='w')
         self.ptt_enable = tk.Checkbutton(hotkey_frame, variable=self.master.ptt_enable_var, command=self.master.toggle_ptt_enable)
@@ -35,6 +36,12 @@ class SettingsWindow(tk.Toplevel):
         tk.Label(hotkey_frame, text="VoIP 'Push To Talk' hotkey(s):", justify='left').grid(column=0, row=2, sticky='w')
         self.ptt = HotkeyEntry(hotkey_frame, clearable=True, stored=self.master.hotkey_listener.get_hotkey('ptt', HotkeyScope.GLOBAL)._keys, stop_callback=lambda x: self.bind_global_hotkey('ptt', x, None), textvariable=self.master.ptt_var)
         self.ptt.grid(column=1, row=2, sticky='ew')
+        self.ptt.bind('<Button>', lambda x: self.stop_other_entries(self.ptt), '+')
+
+    def stop_other_entries(self, selected):
+        for entry in {self.ptt, self.stopall}:
+            if entry is not selected:
+                entry.stop()
 
     def bind_global_hotkey(self, index, hkentry:HotkeyEntry, command):
         hotkey = hkentry.get_hotkey(command)
