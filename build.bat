b0023d1155b846ba0a382dc429ba9b9f31bad070
retrontology <retrontology@hotmail.com>
2021-08-26 15:30:06 -0500

    better windows build system

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 python -m venv .
-.\Scripts\activate
+.\Scripts\Activate.ps1
 pip install -r requirements.txt
-pip install nuitka zstandard
-python -m nuitka --onefile --mingw64 --windows-disable-console --remove-output --assume-yes-for-downloads --follow-imports --plugin-enable=tk-inter --plugin-enable=numpy --include-package pynput --include-data-file=lib\site-packages\_sounddevice_data\portaudio-binaries\libportaudio64bit.dll=_sounddevice_data/portaudio-binaries/ --include-data-file=icon/RB.png=icon/RB.png --windows-icon-from-ico=icon/RB.ico retroboard.py
+pip install pyinstaller
+pyinstaller -y --clean --onefile --add-data 'icon\RB.png;icon' --icon icon\RB.ico retroboard.py
+
+::pip install nuitka zstandard
+::python -m nuitka --onefile --mingw64 --windows-disable-console --remove-output --assume-yes-for-downloads --follow-imports --plugin-enable=tk-inter --plugin-enable=numpy --include-package pynput --include-data-file=lib\site-packages\_sounddevice_data\portaudio-binaries\libportaudio64bit.dll=_sounddevice_data/portaudio-binaries/ --include-data-file=icon/RB.png=icon/RB.png --windows-icon-from-ico=icon/RB.ico retroboard.py
