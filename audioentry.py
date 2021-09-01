c01c4b0e4101c47b050a5574173d273d7877013c
Retrontology <retrontology@hotmail.com>
2021-08-31 23:24:43 -0500

    working libav conversion

--- audioentry.py
+++ audioentry.py
@@ -1,70 +1,36 @@
-import av
 import sounddevice
 import numpy as np
 from threading import Thread
 from functools import partial
-from errorwindow import ErrorWindow
+from avbuffer import AVBuffer
 
 class AudioEntry():
     
     def __init__(self, path:str, parent):
         self.parent = parent
-        self.path = path
         self.gain = dict()
         self.frame_index = dict()
         self.streams = dict()
         self.stop = False
-        self.audio_container = av.open(self.path)
-        self.frames = None
+        self.buffer = AVBuffer(path)
     
-    def load_audio(self):
-        self.frames = []
-        for frame in self.audio_container.decode(audio=0):
-            self.frames.append(frame)
-        self.get_channels()
-        #self.detect_max_channels()
-    
-    def get_channels(self):
-        self.channels = len(self.frames[0].layout.channels)
-
-    def get_audio_stream(self):
-        return self.audio_container.streams.get(audio=0)[0]
-
-    """ def detect_max_channels(self):
-        device_indexes = self.parent.get_devices()
-        max_channels = self.audio_container.
-        for index, gain in device_indexes:
-            c = sounddevice.query_devices(index)['max_output_channels']
-            if c < max_channels:
-                max_channels = c
-        if max_channels < self.segment.channels:
-            self.segment = self.segment.set_channels(max_channels) """
-    
-    def clear_audio(self):
-        self.frames = None
+    def __del__(self):
+        del self.buffer
     
     def playback_callback(self, device_index, outdata, frame_count, time_info, status):
         if status:
             print(status)
         if self.stop:
             raise sounddevice.CallbackStop
-        remainder = len(self.frames) - self.frame_index[device_index]
-        if remainder < 1:
-            raise sounddevice.CallbackStop
-        gain = self.gain[device_index].get()
-        valid_frames = frame_count if remainder >= frame_count else remainder
         
-        data = self.frames[self.frame_index[device_index]].to_ndarray()
-        if remainder > 1:
-            for frame in self.frames[self.frame_index[device_index]+1:self.frame_index[device_index]+valid_frames]:
-                data = np.append(data, frame.to_ndarray(), 1)
-        data = np.transpose(data)
-
+        data = self.buffer.read(frame_count)
+        if len(data) < frame_count:
+            frame_count = len(data)
+            self.stop = True
+        gain = self.gain[device_index].get()
         data = np.vectorize(lambda x: apply_gain(x, gain))(data)
-        outdata[:valid_frames] = data[:valid_frames]
-        outdata[valid_frames:] = 0
-        self.frame_index[device_index] += valid_frames
-        print(outdata)
+        outdata[:frame_count] = data[:frame_count]
+        outdata[frame_count:] = 0
     
     def playback_finished(self, device_index):
         self.streams.pop(device_index)
@@ -72,7 +38,6 @@ class AudioEntry():
         if len(self.streams) == 0:
             if self in self.parent.playing:
                 self.parent.playing.remove(self)
-            self.clear_audio()
             del self
 
     def play(self):
@@ -80,15 +45,12 @@ class AudioEntry():
         self.playback_thread.start()
 
     def _play(self):
-        if not self.frames:
-            self.load_audio()
         device_index = 0
         for device, gain in self.parent.get_devices():
             self.gain[device_index] = gain
             self.frame_index[device_index] = 0
             try:
-                print(self.frames[0].sample_rate)
-                output = sounddevice.OutputStream(callback=partial(self.playback_callback, device_index), finished_callback=partial(self.playback_finished, device_index), device=device, channels=self.channels, samplerate=self.frames[0].sample_rate, dtype=self.frames[0].to_ndarray().dtype)
+                output = sounddevice.OutputStream(callback=partial(self.playback_callback, device_index), finished_callback=partial(self.playback_finished, device_index), device=device, channels=self.buffer.channels, samplerate=self.buffer.sample_rate, dtype=self.buffer.dtype)
                 self.streams[device_index] = output
                 output.start()
                 device_index += 1
---
+++ avbuffer.py
@@ -0,0 +1,36 @@
+import av
+import numpy as np
+
+class AVBuffer():
+
+    def __init__(self, path):
+        self.path = path
+        self.exhausted = False
+        self.init_buffer()
+    
+    def __del__(self):
+        del self._buffer
+    
+    def init_buffer(self):
+        self._frames = av.open(self.path).decode(audio=0)
+        frame = self._frames.__next__()
+        self.channels = len(frame.layout.channels)
+        self.sample_rate = frame.sample_rate
+        frame = frame.to_ndarray()
+        self.dtype = frame.dtype
+        self._buffer = np.transpose(frame)
+
+    def read(self, size=0):
+        if self.exhausted:
+            raise Exception('The file has already been read!')
+        while size > len(self._buffer) or size == 0:
+            try: 
+                self._buffer = np.append(self._buffer, np.transpose(self._frames.__next__().to_ndarray()), 0)
+            except StopIteration as e:
+                self.exhausted = True
+                break
+        if size > len(self._buffer):
+            size = len(self._buffer)
+        out = self._buffer[:size]
+        self._buffer = self._buffer[size:]
+        return out
