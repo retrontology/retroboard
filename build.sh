6b09223dfd6b71c3f35ed930b43f5383c8b8d942
retrontology <retrontology@hotmail.com>
2021-09-01 20:32:27 -0500

    switch to pyinstaller until nuitka can be figured out

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 export TK_LIBRARY=/usr/lib/tk8.6
 export TCL_LIBRARY=/usr/lib/tcl8.6
 
 python -m venv .
 source bin/activate
 pip install -r requirements.txt
-pip install nuitka
-python -m nuitka --onefile --remove-output --assume-yes-for-downloads --follow-imports --plugin-enable=tk-inter --plugin-enable=numpy --include-package pynput --include-package Xlib --include-data-file=icon/RB.png=icon/RB.png --linux-onefile-icon=icon/RB.xpm retroboard.py
+pip install pyinstaller
+pyinstaller -y --clean --onefile --noconsole --add-data "icon/RB.png:icon" --icon icon/RB.xpm retroboard.py
+#pip install nuitka
+#python -m nuitka --onefile --remove-output --assume-yes-for-downloads --follow-imports --plugin-enable=tk-inter --plugin-enable=numpy --include-package pynput --include-package Xlib --include-data-file=icon/RB.png=icon/RB.png --linux-onefile-icon=icon/RB.xpm retroboard.py
