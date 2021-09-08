8a9b4c16e5545bf3bbd745ce76abcadba90bbbef
retrontology <retrontology@hotmail.com>
2021-09-08 03:20:25 -0500

    include all files for av in linux build script

--- build.sh
+++ build.sh
@@ -16,6 +16,6 @@ python -m venv .
 source bin/activate
 pip install -r requirements.txt
 pip install pyinstaller
-pyinstaller -y --clean --onefile --noconsole --add-data "icon/RB.png:icon" --icon icon/RB.xpm retroboard.py
+pyinstaller -y --clean --onefile --noconsole --collect-all av --add-data "icon/RB.png:icon" --icon icon/RB.xpm retroboard.py
 #pip install nuitka
 #python -m nuitka --onefile --remove-output --assume-yes-for-downloads --follow-imports --plugin-enable=tk-inter --plugin-enable=numpy --include-package pynput --include-package Xlib --include-data-file=icon/RB.png=icon/RB.png --linux-onefile-icon=icon/RB.xpm retroboard.py
